1f7594f69dcf044d240fb866c0b2ffb6c6185a3c
Helio Joaquim <hjrcc@hcec.com.br>
2023-03-30 15:56:42 -0300

    Script para criação da tabela no DynamoDB

---
+++ criar_tabela.py
@@ -0,0 +1,22 @@
+import boto3
+dynamodb = boto3.resource('dynamodb','sa-east-1')
+
+table = dynamodb.create_table(
+    TableName='ConfiMusic2',
+    KeySchema=[
+        {
+            'AttributeName': 'artist_name',
+            'KeyType': 'HASH'
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'artist_name',
+            'AttributeType': 'S'
+        }
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5
+    }
+)
